0f18e80fbce9867fbf1a2500e5dc2e904d2aa26b
Bruno Silva <brunovinicius0704@gmail.com>
2024-01-13 18:47:52 -0300

    votes tests

--- src/app/pages/room/votes/votes.component.spec.ts
+++ src/app/pages/room/votes/votes.component.spec.ts
@@ -1,16 +1,23 @@
-import { ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { VotesComponent } from './votes.component';
 import { AngularFireModulesMock, AngularFireProvidersMock } from 'src/app/shared/mocks/others';
 import { RoomService } from 'src/app/services/room.service';
-import { RoomMock, TaskFlipMock, TaskMock } from 'src/app/shared/mocks/constants';
+import { RoomTaskNoVotesMock} from 'src/app/shared/mocks/constants';
 import { CardComponentSpec } from 'src/app/shared/mocks/components/card.component.spec';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RoomServiceProviderMock } from 'src/app/shared/mocks/service';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
+import { CardComponent } from 'src/app/shared/components/card/card.component';
 
-xdescribe('VotesComponent', () => {
+describe('VotesComponent', () => {
   let component: VotesComponent;
   let fixture: ComponentFixture<VotesComponent>;
   let service: RoomService;
+  let actions: DebugElement;
+  let result: DebugElement;
+  let currentVotes: DebugElement;
+  let votes: DebugElement;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -29,77 +36,106 @@ xdescribe('VotesComponent', () => {
     });
     fixture = TestBed.createComponent(VotesComponent);
     component = fixture.componentInstance;
-
     service = TestBed.inject(RoomService);
-    component.room = RoomMock;
-    fixture.detectChanges();
+
+    service.updateRoom(RoomTaskNoVotesMock);
+    service.listenerRoom('path').subscribe((room) => {
+      component.room = room;
+      checkChanges();
+    });
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+  describe('on Init', () => {
+    it('should create', () => {
+      expect(component).toBeTruthy();
+      expect(actions).withContext('actions should NOT exist on HTML').not.toBeTruthy();
+      expect(result).withContext('result should exist on HTML').toBeTruthy();
+      expect(currentVotes).withContext('currentVotes should exist on HTML').toBeTruthy();
+      expect(votes).withContext('votes should exist on HTML').toBeTruthy();
+    });
+  
+    it('results should be "-"', () => expect(result.nativeElement.textContent).toBe('-'));
+    it('should have 0 current votes', () => expect(currentVotes.children.length).toBe(0));
+    describe('should have a fribbonacci cards', () => {
+      type FribonnacciTest = { position: number, value: number };
+      let fribonnacciTests: FribonnacciTest[] = [
+        { position: 0, value: 1 },
+        { position: 1, value: 2 },
+        { position: 2, value: 3 },
+        { position: 3, value: 5 },
+        { position: 4, value: 8 },
+        { position: 5, value: 13 },
+        { position: 6, value: 21 },
+        { position: 7, value: 34 },
+        { position: 8, value: 55 },
+        { position: 9, value: 89 }
+      ];
+      it('length should be 10', () => expect(votes.children.length).toBe(10));
+      fribonnacciTests.forEach(test => {
+        it(`position ${test.position} should have value ${test.value}`, () => {
+          const card: CardComponent = votes.children[test.position].componentInstance;
+          expect(card.card.value).toBe(test.value);
+        });
+      });
+    });
+  })
+  
 
-  describe('on', () => {
-    let updateRoomSpy: jasmine.Spy;
-    let taskCallsSpy: {
-      vote: jasmine.Spy,
-      showVotes: jasmine.Spy,
-      revote: jasmine.Spy
-    };
+  describe('after vote', () => {
+    let voteSpy: jasmine.Spy;
     beforeEach(() => {
-      updateRoomSpy = spyOn(service, 'updateRoom');
-      taskCallsSpy = {
-        vote: spyOn(component.task, 'vote').and.callThrough(),
-        showVotes: spyOn(component.task, 'showVotes').and.callThrough(),
-        revote: spyOn(component.task, 'revote').and.callThrough()
-      }
+      voteSpy = spyOn(component.task, 'vote').and.callThrough();
+      votes.children[0].triggerEventHandler('click', null);
+      checkChanges();
     });
-    describe('try to vote', () => {
-      describe('when has flipped cards', () => {
-        afterEach(() => expect(updateRoomSpy).not.toHaveBeenCalled());
-        beforeEach(fakeAsync(() => {
-          component.room.tasks = [TaskFlipMock];
-          component.vote(1);
-          tick(100);
-        }));
-        it('should not call task.vote', () => expect(taskCallsSpy.vote).not.toHaveBeenCalled());
+
+    it('actions should appear', () => expect(actions).withContext('actions should exist on HTML').toBeTruthy());
+    it('actions should have 2 buttons', () => expect(actions.children.length).toBe(2));
+    it('first button should have text "Virar cartas"', () => expect(actions.children[0].nativeElement.textContent).toBe('Virar cartas'));
+    it('second button should have text "Revotar"', () => expect(actions.children[1].nativeElement.textContent).toBe('Revotar'));
+    it('result should be "-"', () => expect(result.nativeElement.textContent).toBe('-'));
+    it('should have 1 current votes', () => expect(currentVotes.children.length).toBe(1));
+
+    describe('after show votes', () => {
+      let spyUpdateRoom: jasmine.Spy;
+      beforeEach(() => {
+        actions.children[0].triggerEventHandler('click', null);
+        checkChanges();
+        spyUpdateRoom = spyOn(service, 'updateRoom').and.callThrough();
       });
-      describe('when has no flipped cards', () => {
-        afterEach(() => expect(updateRoomSpy).toHaveBeenCalledTimes(1));
-        beforeEach(fakeAsync(() => {
-          component.room.tasks = [TaskMock];
-          component.vote(1);
-          tick(100);
-        }));
-        it('should call task.vote', () => expect(taskCallsSpy.vote).toHaveBeenCalledTimes(1));
+      it('flipped should be true', () => expect(component.flippedVotes).toBeTrue());
+      it('should hide fribonnacci votes', () => expect(votes).withContext('votes should NOT exist on HTML').not.toBeTruthy());
+      it('result should be "1"', () => expect(result.nativeElement.textContent).toBe('1'));
+      it('on try to vote again, should not be possible', () => {
+        component.vote(1);
+        expect(voteSpy).not.toHaveBeenCalled();
+        expect(spyUpdateRoom).not.toHaveBeenCalled();
+      });
+      it('on try to show votes again, should not be possible', () => {
+        component.showVotes();
+        expect(spyUpdateRoom).not.toHaveBeenCalled();
       });
     });
 
-    describe('try to show votes', () => {
-      describe('when has flipped cards', () => {
-        afterEach(() => expect(updateRoomSpy).not.toHaveBeenCalled());
-        beforeEach(() => {
-          component.room.tasks = [TaskFlipMock];
-          component.showVotes();
-        });
-        it('should not call task.showVotes', () => expect(taskCallsSpy.showVotes).not.toHaveBeenCalled());
+    describe('after revote', () => {
+      beforeEach(() => {
+        actions.children[1].triggerEventHandler('click', null);
+        checkChanges();
       });
-      describe('when has no flipped cards', () => {
-        afterEach(() => expect(updateRoomSpy).toHaveBeenCalledTimes(1));
-        beforeEach(() => {
-          component.room.tasks = [TaskMock];
-          component.showVotes()
-        });
-        it('should call task.showVotes', () => expect(taskCallsSpy.showVotes).toHaveBeenCalledTimes(1));
-      });
-    });
-
-    describe('try to revote', () => {
-      afterEach(() => expect(updateRoomSpy).toHaveBeenCalledTimes(1));
-      beforeEach(() => component.revote());
-      it('should call task.revote', () => expect(taskCallsSpy.revote).toHaveBeenCalledTimes(1));
+      it('flipped should be false', () => expect(component.flippedVotes).toBeFalse());
+      it('should show fribonnacci votes', () => expect(votes).withContext('votes should exist on HTML').toBeTruthy());
+      it('result should be "-"', () => expect(result.nativeElement.textContent).toBe('-'));
+      it('current votes should be empty', () => expect(currentVotes.children.length).toBe(0));
     });
   });
+
+  function checkChanges() : void {
+    fixture.detectChanges();
+    actions = fixture.debugElement.query(By.css('.actions'));
+    result = fixture.debugElement.query(By.css('.votes-result>h1'));
+    currentVotes = fixture.debugElement.query(By.css('.current-votes-list'));
+    votes = fixture.debugElement.query(By.css('.votes'));
+  }
 });
 
 
--- src/app/shared/mocks/constants/index.ts
+++ src/app/shared/mocks/constants/index.ts
@@ -1,13 +1,35 @@
+import { IRoom } from "src/app/pages/room/interfaces/room.interface";
+import { ITask } from "src/app/pages/room/interfaces/task.interface";
+import { IVote } from "src/app/pages/room/interfaces/vote.interface";
 import { Room } from "src/app/pages/room/models/room";
 import { Task } from "src/app/pages/room/models/task";
 import { Vote } from "src/app/pages/room/models/vote";
+import { IRoomItem } from "src/app/pages/rooms/room-item/interfaces/room-item.inteface";
 import { RoomItem } from "src/app/pages/rooms/room-item/models/room-item";
 
 const any: any = jasmine.anything();
-export const VoteMock = new Vote('mocked user id 1', 'mocked user name 1', 10);
-export const VoteFlipMock = new Vote('mocked user id 2', 'mocked user name 2', 10, false);
-export const TaskMock = new Task('mocked task 1', [VoteMock])
-export const TaskFlipMock = new Task('mocked task 2', [VoteFlipMock])
-export const RoomMock = new Room('mocked room', 'mocked creator', [TaskMock, TaskFlipMock], 0, 'mocked id');
-export const RoomEmptyMock = new Room('mocked room', 'mocked creator', [], 0, 'mocked id');
-export const RoomItemMock = new RoomItem(RoomMock.id!, RoomMock.name, any, any);
+export class ConstantMocks {
+  static getTaskMock(name: string, votes: IVote[] = []): ITask {
+    return new Task(name, votes);
+  }
+  static getVoteMock(id: string, user: string, value: number = 10, hidden: boolean = true): IVote {
+    return new Vote(id, user, value, hidden);
+  }
+  static getRoomMock(name: string, creator: string, tasks: ITask[] = [], currentTask: number = 0, id: string = 'mocked id'): IRoom {
+    return new Room(name, creator, tasks, currentTask, id);
+  }
+  static getRoomItemMock(room: IRoom): IRoomItem {
+    return new RoomItem(room.id!, room.name, any, any);
+  }
+}
+
+export const VoteMock = ConstantMocks.getVoteMock('1', 'user 1', 10, true);
+export const VoteFlipMock = ConstantMocks.getVoteMock('1', 'user 1', 10, false);
+export const TaskNoVotesMock = ConstantMocks.getTaskMock('mocked task 1');
+export const TaskMock = ConstantMocks.getTaskMock('mocked task 2', [VoteMock]);
+export const TaskFlipMock = ConstantMocks.getTaskMock('mocked task 3', [VoteFlipMock]);
+export const RoomEmptyMock = ConstantMocks.getRoomMock('mocked room', 'mocked creator');
+export const RoomTaskNoVotesMock = ConstantMocks.getRoomMock('mocked room', 'mocked creator', [TaskNoVotesMock], 0, 'mocked id');
+export const RoomTaskAndVotesMock = ConstantMocks.getRoomMock('mocked room', 'mocked creator', [TaskMock], 0, 'mocked id');
+export const RoomMock = ConstantMocks.getRoomMock('mocked room', 'mocked creator', [TaskMock, TaskFlipMock], 0, 'mocked id');
+export const RoomItemMock = ConstantMocks.getRoomItemMock(RoomMock);
