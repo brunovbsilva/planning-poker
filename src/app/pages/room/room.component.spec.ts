df34efdcafb962118aa07e699955dbd773f6f896
Bruno Silva <brunovinicius0704@gmail.com>
2024-01-13 10:58:15 -0300

    room tests

--- src/app/pages/room/room.component.spec.ts
+++ src/app/pages/room/room.component.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
 
 import { RoomComponent } from './room.component';
 import { RouterTestingModule } from '@angular/router/testing';
@@ -6,11 +6,22 @@ import { RoomService } from 'src/app/services/room.service';
 import { TasksComponentSpec } from 'src/app/shared/mocks/pages/tasks.component.spec';
 import { VotesComponentSpec } from 'src/app/shared/mocks/pages/votes.component.spec';
 import { RoomServiceProviderMock } from 'src/app/shared/mocks/service';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
+import { RoomEmptyMock, RoomMock } from 'src/app/shared/mocks/constants';
+import { TasksComponent } from './tasks/tasks.component';
+import { VotesComponent } from './votes/votes.component';
 
 describe('RoomComponent', () => {
   let component: RoomComponent;
   let fixture: ComponentFixture<RoomComponent>;
   let service: RoomService;
+  let spyListener: jasmine.Spy;
+  let spyUpdate: jasmine.Spy;
+  let room: DebugElement;
+  let name: HTMLElement;
+  let tasks: TasksComponent;
+  let votes: VotesComponent;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -25,17 +36,66 @@ describe('RoomComponent', () => {
     fixture = TestBed.createComponent(RoomComponent);
     component = fixture.componentInstance;
     service = TestBed.inject(RoomService);
-    fixture.detectChanges();
+    spyListener = spyOn(service, 'listenerRoom').and.callThrough();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-    expect(component.room).toBeTruthy();
-  });
-
-  it('should update room', () => {
-    let spyUpdate = spyOn(component.room!, 'updateValues').and.callThrough();
-    service.updateRoom(component.room!);
-    expect(spyUpdate).toHaveBeenCalled();
+  describe('on Init', () => {
+    beforeEach(() => {
+      service.updateRoom(RoomMock);
+      fixture.detectChanges();
+      room = fixture.debugElement.query(By.css('.current-room'));
+    });
+  
+    it('should create', () => {
+      expect(component).toBeTruthy();
+      expect(component.room).toBeTruthy();
+      expect(room).withContext('room HTML should exist').toBeTruthy();
+    });
+    
+    it('should start listener room', fakeAsync(() => {
+      tick(100);
+      expect(spyListener).toHaveBeenCalled();
+    }));
+  
+    it('should update values on next value', () => {
+      spyUpdate = spyOn(component.room!, 'updateValues').and.callThrough();
+      service.updateRoom(RoomMock);
+      expect(spyUpdate).toHaveBeenCalled();
+      expect(component.room?.id).withContext('id from room should be equal mocked').toEqual(RoomMock.id);
+      expect(component.room?.name).withContext('name from room should be equal mocked').toEqual(RoomMock.name);
+      expect(component.room?.creator).withContext('creator from room should be equal mocked').toEqual(RoomMock.creator);
+      expect(component.room?.currentTask).withContext('currentTask from room should be equal mocked').toEqual(RoomMock.currentTask);
+      expect(component.room?.tasks).withContext('tasks from room should be equal mocked').toEqual(RoomMock.tasks);
+    });
   })
+
+  describe('if room has tasks', () => {
+    beforeEach(() => {
+      service.updateRoom(RoomMock);
+      fixture.detectChanges();
+      room = fixture.debugElement.query(By.css('.current-room'));
+      name = room.nativeElement.querySelector('.room-name');
+      tasks = room.nativeElement.querySelector('app-tasks');
+      votes = room.nativeElement.querySelector('app-votes');
+    });
+
+    it('should show room name', () => expect(room.nativeElement.textContent).withContext('Room name').toContain(RoomMock.name));
+    it('should show tasks', () => expect(tasks).withContext('app-tasks HTML').toBeTruthy());
+    it('should show votes', () => expect(votes).withContext('app-votes HTML').toBeTruthy());
+  });
+
+  describe('if room has no tasks', () => {
+    beforeEach(() => {
+      service.updateRoom(RoomEmptyMock);
+      fixture.detectChanges();
+      room = fixture.debugElement.query(By.css('.current-room'));
+      name = room.nativeElement.querySelector('.room-name');
+      tasks = room.nativeElement.querySelector('app-tasks');
+      votes = room.nativeElement.querySelector('app-votes');
+    });
+
+    it('should show room name', () => expect(room.nativeElement.textContent).withContext('Room name').toContain(RoomMock.name));
+    it('should not show tasks', () => expect(tasks).withContext('app-tasks HTML').toBeTruthy());
+    it('should not show votes', () => expect(votes).withContext('app-votes HTML').toBeFalsy());
+  });
 });
--- src/app/shared/mocks/constants/index.ts
+++ src/app/shared/mocks/constants/index.ts
@@ -4,9 +4,10 @@ import { Vote } from "src/app/pages/room/models/vote";
 import { RoomItem } from "src/app/pages/rooms/room-item/models/room-item";
 
 const any: any = jasmine.anything();
-export const VoteMock = new Vote('mocked user id', 'mocked user name', 10);
-export const VoteFlipMock = new Vote('mocked user id', 'mocked user name', 10, false);
-export const TaskMock = new Task('mocked task', [VoteMock, VoteMock])
-export const TaskFlipMock = new Task('mocked task', [VoteFlipMock, VoteMock])
+export const VoteMock = new Vote('mocked user id 1', 'mocked user name 1', 10);
+export const VoteFlipMock = new Vote('mocked user id 2', 'mocked user name 2', 10, false);
+export const TaskMock = new Task('mocked task 1', [VoteMock])
+export const TaskFlipMock = new Task('mocked task 2', [VoteFlipMock])
 export const RoomMock = new Room('mocked room', 'mocked creator', [TaskMock, TaskFlipMock], 0, 'mocked id');
+export const RoomEmptyMock = new Room('mocked room', 'mocked creator', [], 0, 'mocked id');
 export const RoomItemMock = new RoomItem(RoomMock.id!, RoomMock.name, any, any);
