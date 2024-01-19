eebcbf5bad3b914d0019be0f030789eda9d6b6a5
Bruno Silva <brunovinicius0704@gmail.com>
2024-01-19 19:17:49 -0300

    adjust

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,11 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: "AIzaSyAeN6DAueQxYzVMBF2cGv2olFE_Kf9Qc58",
-    authDomain: "planning-poker-343c7.firebaseapp.com",
-    projectId: "planning-poker-343c7",
-    storageBucket: "planning-poker-343c7.appspot.com",
-    messagingSenderId: "26931702551",
-    appId: "1:26931702551:web:0c337c6a1415edd3e2e552",
-    measurementId: "G-S2Q7EM5GE7"
+    apiKey: "AIzaSyBxcb1o6HFEHy-RwZevt909OKSOqEpvW-g",
+    authDomain: "planning-poker-cd302.firebaseapp.com",
+    projectId: "planning-poker-cd302",
+    storageBucket: "planning-poker-cd302.appspot.com",
+    messagingSenderId: "903421093759",
+    appId: "1:903421093759:web:79ee181c62d36ca5d09c4a"
   }
 }
