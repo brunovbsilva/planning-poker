de7ce6f71532f7bb6fabf1ac5b531e4f31872701
Bruno Silva <brunovinicius0704@gmail.com>
2024-07-20 16:15:57 -0300

    update angular fire

--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,23 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-
 import { AppComponent } from './app/app.component';
 import { isDevMode, importProvidersFrom } from '@angular/core';
 import { ServiceWorkerModule } from '@angular/service-worker';
-import { AngularFireStorageModule } from '@angular/fire/compat/storage';
-import { AngularFireAuthModule } from '@angular/fire/compat/auth';
-import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 import { environment } from 'src/environments/environment';
-import { AngularFireModule } from '@angular/fire/compat';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
 import { TitlePrefixStrategy } from './app/shared/strategies/title-prefix.strategy';
 import {provideRouter, TitleStrategy} from '@angular/router';
 import {routes} from "./app/app-routing";
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 
 
 bootstrapApplication(AppComponent, {
   providers: [
     importProvidersFrom(
       BrowserModule,
-      AngularFireModule.initializeApp(environment.firebaseConfig),
-      AngularFirestoreModule,
-      AngularFireAuthModule,
-      AngularFireStorageModule,
       ServiceWorkerModule.register(
         'ngsw-worker.js', {
         enabled: !isDevMode(),
@@ -32,7 +26,12 @@ bootstrapApplication(AppComponent, {
     ),
     provideAnimations(),
     provideRouter(routes),
-    { provide: TitleStrategy, useClass: TitlePrefixStrategy }
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideDatabase(() => getDatabase()),
+    provideStorage(() => getStorage()),
+    { provide: TitleStrategy, useClass: TitlePrefixStrategy },
   ]
 })
   .catch(err => console.error(err));
